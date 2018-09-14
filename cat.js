91694021e2c986360da7f5b227e48042408e6b78
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-09-14 09:15:59 +0200

    Applying red background in diagonal with pure JS

--- cat.js
+++ cat.js
@@ -1,18 +1,19 @@
 const image = document.getElementById('cat');
-const image2 = document.getElementById('cat2');
 const counter = document.getElementById('counter');
 const counterWrapper = document.getElementById('counter-wrapper');
 const counter2 = document.getElementById('counter2');
-const counterWrapper2 = document.getElementById('counter-wrapper2');
-const cat1 = "Georges";
-const cat2 = "Henry";
-const cat1name = document.getElementById('cat1name');
-const cat2name = document.getElementById('cat2name');
+const cats = ["Georges", "Franck", "Alphonse", "Henry", "Hermine"];
+const list = document.querySelector('select');
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  cat1name.innerHTML = cat1;
-  cat2name.innerHTML = cat2;
+  cats.forEach(cat => {
+    list.insertAdjacentHTML('afterbegin',
+    `<option value=${cat}>${cat}</option>`)
+  })
 });
+
+
 
 if(image){
   let x = 0;
@@ -25,15 +26,5 @@ if(image){
   } )
 }
 
-if(image2){
-  let x = 0;
-  counterWrapper2.innerHTML = 'You haven\'t clicked me yet!';
-  image2.addEventListener('click', () => {
-    x++;
-    console.log(x);
-    counter2.innerHTML = x;
-    counterWrapper2.innerHTML = "You've already clicked me " + x + (x > 1 ? " times" : " time");
-  } )
-}
-
 console.log("running from cat.js");
+
