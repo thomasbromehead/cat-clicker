68f330f251a3fc24ed76500af61159246020a8af
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-09-24 16:09:16 +0200

    Recursion through a linked list

--- cat.js
+++ cat.js
@@ -1,11 +1,37 @@
-const fibonacci = (n) => {
-  if (n === 0){
-    return 0;
-  } else if (n === 1) {
-    return 1 ;
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+
+const PrintList = (list) => {
+  if (list.next === null) {
+    console.log(list.value)
   } else {
-    return fibonacci(n-1) + fibonacci(n-2)
+    console.log(list.value)
+    return PrintList(list.next)
   }
 }
 
-console.log(fibonacci(6));
+// Alternative
+
+function printList(list) {
+
+  alert(list.value); // output the current item
+
+  if (list.next) {
+    printList(list.next); // do the same for the rest of the list
+  }
+
+}
+
+printList(list);
