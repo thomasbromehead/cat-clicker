00897f115c2ac3236439ff0b4974d52595dfb355
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-09-24 11:45:00 +0200

    Simplest recursion case

--- cat.js
+++ cat.js
@@ -1,30 +1,9 @@
-const image = document.getElementById('cat');
-const counter = document.getElementById('counter');
-const counterWrapper = document.getElementById('counter-wrapper');
-const counter2 = document.getElementById('counter2');
-const cats = ["Georges", "Franck", "Alphonse", "Henry", "Hermine"];
-const list = document.querySelector('select');
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  cats.forEach(cat => {
-    list.insertAdjacentHTML('afterbegin',
-    `<option value=${cat}>${cat}</option>`)
-  })
-});
-
-
-
-if(image){
-  let x = 0;
-  counterWrapper.innerHTML = 'You haven\'t clicked me yet!';
-  image.addEventListener('click', () => {
-    x++;
-    console.log(x);
-    counter.innerHTML = x;
-    counterWrapper.innerHTML = "You've already clicked me " + x + (x > 1 ? " times" : " time");
-  } )
+const recursive = (n) => {
+  if (n === 1) {
+    return 1;
+  } else {
+    return n + recursive(n-1)
+  }
 }
 
-console.log("running from cat.js");
-
+console.log(recursive(1));
