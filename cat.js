73c66d7d68583320aff5780686fcc58831864724
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-09-24 13:10:05 +0200

    Fibonacci with recursion, slow, crahs prone version

--- cat.js
+++ cat.js
@@ -1,9 +1,11 @@
-const factorial = (n) => {
-  if (n === 1){
-    return 1;
+const fibonacci = (n) => {
+  if (n === 0){
+    return 0;
+  } else if (n === 1) {
+    return 1 ;
   } else {
-    return n * factorial(n-1);
+    return fibonacci(n-1) + fibonacci(n-2)
   }
 }
 
-console.log(factorial(5));
+console.log(fibonacci(6));
